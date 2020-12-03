f5cbcb5d22647a36a69134ab7a6509d7fb0b375e
danielkwon89 <danielkwon89@gmail.com>
2020-12-03 12:10:52 -0500

    commit tree 7c749c3b87ee96a5a2ff4e1191f748228324e54b

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const map = (arr, callback) => {
 //     return arr.reduce(callback, initVal)
 // }
 
+// function reduce(arr, cb, initVal = 0){
+//     return arr.reduce(cb, initVal)
+// }
+
 function reduce(arr, cb, initVal = 0){
-    return arr.reduce(cb, initVal)
+    let r = (!!initVal) ? initVal : arr[0]
+    let i = (!!initVal) ? 0 : 1
+  
+    for (; i < arr.length; i++) {
+      r = cb(arr[i], r)
+    }
+
+    return r;
 }
